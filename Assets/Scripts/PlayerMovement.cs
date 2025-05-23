b35dce956c19d6f378e08c33e8c32c2c77133060
fragmepls <leo.oberrauch@gmail.com>
2025-05-24 01:34:40 +0200

    Added border

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 
+// ReSharper disable All
+
 public class PlayerMovement : MonoBehaviour
 {
-    
     [SerializeField] private float _moveSpeed = 5f;
-    
+
     private Vector2 _movement;
-    
     private Rigidbody2D _rb;
     private Animator _animator;
-    
-    private const string _horizontal        = "Horizontal";
-    private const string _vertical          = "Vertical";
-    private const string _lastHorizontal    = "LastHorizontal";
-    private const string _lastVertical      = "LastVertical";
-    
+
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -24,24 +24,14 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
-        
+
         _rb.linearVelocity = _movement.normalized * _moveSpeed;
-        
+
         _animator.SetFloat(_horizontal, _movement.x);
         _animator.SetFloat(_vertical, _movement.y);
 
         if (_movement == Vector2.zero) return;
         _animator.SetFloat(_lastHorizontal, _movement.x);
         _animator.SetFloat(_lastVertical, _movement.y);
-        
-        PreventPlayerGoingOffScreen();
-    }
-    
-    private void PreventPlayerGoingOffScreen()
-    {
-        Vector2 screenPos = Camera.main.WorldToViewportPoint(transform.position);
-        screenPos.x = Mathf.Clamp(screenPos.x, 0.05f, 0.95f);
-        screenPos.y = Mathf.Clamp(screenPos.y, 0.05f, 0.95f);
-        transform.position = Camera.main.ViewportToWorldPoint(screenPos);
     }
 }
